6d81630f1ecda268e1fa6a7db07a2109ce14896f
Shahu D Tarale <Shahu.Tarale@in.ey.com>
2022-09-16 14:39:56 +0530

    First Commit

---
+++ src/test/java/com/Framework_4/TestCases/SampleTestCase.java
@@ -0,0 +1,13 @@
+package com.Framework_4.TestCases;
+
+import org.testng.annotations.Test;
+
+public class SampleTestCase {
+	
+	@Test
+	public void Sampletest()
+	{
+		System.out.println("Running a sample test");
+	}
+
+}
